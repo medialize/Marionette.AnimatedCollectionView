84049acdc5ea3ef43e21436caa742ff99889f06a
Rodney Rehm <rrehm@RodBook.local>
2014-06-23 19:57:29 +0200

    initials

---
+++ README.md
@@ -0,0 +1,59 @@
+# Marionette.AnimatedCollectionView
+
+[MarionetteJS](http://marionettejs.com/) extension to decorate a [CollectionView](https://github.com/marionettejs/backbone.marionette/blob/master/docs/marionette.collectionview.md) with callbacks to allow animated DOM insertion and removal.
+
+## Usage
+
+```js
+var decorateAnimatedCollectionView = require('Marionette.AnimatedCollectionView');
+
+var ListView = Marionette.CollectionView.extend({
+  // …
+});
+
+var AnimatedListView = decorateAnimatedCollectionView(ListView, {
+  // CSS class name to add for animating DOM insertion
+  add: 'item-adding',
+  // CSS class name to add for animating DOM removal
+  remove: 'item-removing',
+  // automatic promise resolution
+  promise: {
+    // options from jQuery-transitionEndEvent
+    resolveTimeout: 1000
+  }
+});
+```
+
+
+## TODO
+
+* documentation and tests
+* API to differentiate between initial render and later additions
+* API to allow "drag" (a delay between each item to animate in)
+
+
+## Install
+
+```bash
+# with bower
+bower install Marionette.AnimatedCollectionView
+
+# with npm
+npm install marionette.animatedcollectionview
+```
+
+**Note:** requires ['jQuery-transitionEndPromise'](https://github.com/medialize/jQuery-transitionEndPromise) to be made available at `jQuery-transitionEndPromise`
+
+
+## License
+
+Marionette.AnimatedCollectionView is published under the [MIT License](http://opensource.org/licenses/mit-license).
+
+
+## Changelog
+
+### 0.1.0 (June 23rd 2014) ###
+
+* Initial Version
+
+
---
+++ src/Marionette.AnimatedCollectionView.js
@@ -0,0 +1,66 @@
+/*! 
+ * Marionette.AnimatedCollectionView v0.1.0
+ * @web: https://github.com/medialize/Marionette.AnimatedCollectionView/
+ * @author: Rodney Rehm - http://rodneyrehm.de/en/
+ */
+(function (root, factory) {
+  'use strict';
+  // https://github.com/umdjs/umd/blob/master/returnExports.js
+  if (typeof exports === 'object') {
+    // Node
+    module.exports = factory(require('jquery', 'jQuery.transitionEndPromise'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery', 'jQuery.transitionEndPromise'], factory);
+  } else {
+    // Browser globals (root is window)
+    factory(root.jQuery);
+  }
+}(this, function ($) {
+  'use strict';
+
+  function decorateAnimatedCollectionView(View, options) {
+      var o = $.extend({}, decorateAnimatedAddRemove.defaults, options || {});
+      var _removeChildView = View.prototype.removeChildView;
+      var _initialize = View.prototype.initialize;
+
+      return View.extend({
+          initialize: function() {
+              this.listenTo(this, 'collection:rendered', function() {
+                  // bind before:item:added after the collection has been fully rendered,
+                  // otherwise the add animation would be triggered for everything
+                  this.listenTo(this, 'before:item:added', this._animateAdd, this);
+              }, this);
+
+              return _initialize.apply(this, arguments);
+          },
+
+          removeChildView: function(view) {
+              this._animateRemove(view)
+                  .then(_removeChildView.bind(this, view));
+          },
+
+          _animateAdd: function(view) {
+              view.$el.addClass(o.add);
+          },
+
+          _animateRemove: function(view) {
+              var promise = view.$el.animationEndPromise(options.promise);
+              view.$el.addClass(o.remove);
+              return promise;
+          }
+      });
+  }
+  
+  decorateAnimatedCollectionView.version = '0.1.0';
+  decorateAnimatedCollectionView.defaults = {
+      add: 'item-adding',
+      remove: 'item-removing',
+      // options from jQuery-transitionEndEvent
+      promise: {
+          resolveTimeout: 1000
+      }
+  };
+  
+  return decorateAnimatedCollectionView;
+}));
